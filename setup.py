da7f6f105b86f7bf55f1987ce62f55954c141b56
blankRiot96 <blankRiot96@gmail.com>
2024-03-24 11:45:44 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import find_packages, setup
 
 VERSION = "0.1.0"
 DESCRIPTION = "Modal Editor"
-LONG_DESCRIPTION = """Modal text editor"""
+LONG_DESCRIPTION = """A Modal Text editor that
+focuses on aesthetics, functionality and usability."""
 
 setup(
     name="axedit",
@@ -17,16 +18,12 @@ setup(
     install_requires=["pygame-ce"],
     python_requires=">=3.11",
     keywords=["editor"],
-    # classifiers=[
-    #     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    #     "Programming Language :: Python :: 3.7",
-    #     "Programming Language :: Python :: 3.8",
-    #     "Programming Language :: Python :: 3.9",
-    #     "Programming Language :: Python :: 3.10",
-    #     "Topic :: Artistic Software",
-    #     "Topic :: Multimedia :: Sound/Audio",
-    #     "Intended Audience :: End Users/Desktop",
-    # ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Text Editors",
+        "Intended Audience :: Developers",
+    ],
     entry_points={"console_scripts": ["axedit=axedit:main", "axe=axedit:main"]},
     include_package_data=True,
     # data_files=[
