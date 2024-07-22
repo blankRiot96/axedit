424ad9ac8c0d6302932535a85640dbe4af924181
blankRiot96 <blankRiot96@gmail.com>
2024-07-22 12:57:22 +0530

    unnecessary commit

---
+++ axedit/editor_interface.py
@@ -0,0 +1,2 @@
+class EditorInterface:
+    """Processes and translates the human input(mouse and key press) to text editing actions"""
---
+++ axedit/live_text_object.py
@@ -0,0 +1,2 @@
+class LiveTextObject:
+    """Represents the text data of each file in memory"""
